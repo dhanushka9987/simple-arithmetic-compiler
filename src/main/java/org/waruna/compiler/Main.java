d9c862129ea3f2dd246223205dff8238f28d5dcd
dhanushka9987 <ndanushka16889@email.com>
2025-02-02 14:44:06 +0530

    Run And Debug

--- src/main/java/org/waruna/compiler/Main.java
+++ src/main/java/org/waruna/compiler/Main.java
@@ -1,8 +1,23 @@
 package org.waruna.compiler;
 
+import java.util.List;
+
+import org.waruna.compiler.model.Node;
+import org.waruna.compiler.model.Token;
+import org.waruna.compiler.parse.Lexer;
+import org.waruna.compiler.parse.Parser;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("This is simple arithmetic compiler");
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar simple-arithmatic-compiler <expression>");
+            return;
+        }
+        Lexer lexer = new Lexer();
+        List<Token> tokens = lexer.tokenize(args[0]);
+        Parser parser = new Parser();
+        Node parseTree = parser.parse(tokens);
+        System.out.println(parseTree);
     }
 }
